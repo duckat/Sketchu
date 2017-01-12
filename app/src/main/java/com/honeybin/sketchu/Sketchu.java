6df095ef1a320cd5cb68d4c8c366a9d5cc118746
klee166 <klee166@jhu.edu>
2017-01-12 00:37:05 -0500

    Sketchu Class with different parameters

---
+++ app/src/main/java/com/honeybin/sketchu/Sketchu.java
@@ -0,0 +1,40 @@
+package com.honeybin.sketchu;
+
+/**
+ * Sketchu class
+ */
+
+public class Sketchu {
+    private int hunger = 0;
+    private int cleanliness = 0;
+    private int love = 0;
+    private int drowsiness = 0;
+
+    // intelligence category
+    private int knowledge = 0;
+    private int creativity = 0;
+    private int comprehensibility = 0;
+    private int musicalAbility = 0;
+
+    // outer appearance
+    private int appearance = 0;
+    private int physicality = 0;
+    private int fitness = 0;
+
+    // social category
+    private int sociability = 0;
+    private int friendliness = 0;
+
+    // mind control
+    private int confidence = 0;
+    private int concentration = 0;
+
+    // personality
+    private int shoppingImpulsiveness = 0;
+    private int otakuness = 0;
+
+    public Sketchu() {
+
+    }
+    
+}
