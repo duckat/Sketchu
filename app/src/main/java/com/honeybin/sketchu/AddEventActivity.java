eafd2366f2ae0b88add311f88e2b040cb8ab43ee
klee166 <klee166@jhu.edu>
2017-01-12 08:28:26 -0500

    Add Event Activity

--- app/src/main/java/com/honeybin/sketchu/AddEventActivity.java
+++ app/src/main/java/com/honeybin/sketchu/AddEventActivity.java
@@ -3,7 +3,7 @@ package com.honeybin.sketchu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class EventActivity extends AppCompatActivity {
+public class AddEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
