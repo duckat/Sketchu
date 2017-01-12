deff3ac5834dd5c6e58b802abd708b5a8dc6a696
klee166 <klee166@jhu.edu>
2017-01-12 03:04:53 -0500

    Event List Button

--- app/src/main/java/com/honeybin/sketchu/Sketchu.java
+++ app/src/main/java/com/honeybin/sketchu/Sketchu.java
@@ -1,7 +1,7 @@
 package com.honeybin.sketchu;
 
 /**
- * Sketchu class
+ * Sketchu class.
  */
 
 public class Sketchu {
@@ -11,109 +11,107 @@ public class Sketchu {
     private int drowsiness = 0;
 
     // intelligence category
-    private int knowledge = 0;
-    private int creativity = 0;
-    private int comprehensibility = 0;
-    private int musicalAbility = 0;
+    private double knowledge = 0.0;
+    private double creativity = 0.0;
+    private double comprehensibility = 0.0;
+    private double musicalAbility = 0.0;
 
     // outer appearance
-    private int appearance = 0;
-    private int physicality = 0;
-    private int fitness = 0;
+    private double appearance = 0.0;
+    private double physicality = 0.0;
+    private double fitness = 0.0;
 
     // social category
-    private int sociability = 0;
-    private int friendliness = 0;
-    private int expressiveness = 0;
+    private double sociability = 0.0;
+    private double friendliness = 0.0;
+    private double expressiveness = 0.0;
 
     // mind control
-    private int confidence = 0;
-    private int concentration = 0;
-    private int sentimentality = 0;
+    private double confidence = 0.0;
+    private double concentration = 0.0;
+    private double sentimentality = 0.0;
 
     // personality
-    private int shoppingImpulsiveness = 0;
-    private int otakuness = 0;
+    private double shoppingImpulsiveness = 0.0;
+    private double otakuness = 0.0;
 
     public Sketchu() {
 
     }
+    private double timeConversion(double minutes) {
+        double timeScore = minutes / 60.0;
+        return timeScore;
+    }
 
-    public void incHunger() {
-
+    public void incHunger(double time) {
     }
 
     public void incCleanliness() {
-
     }
 
     public void incLove() {
-
     }
 
     public void incDrowsiness() {
+    }
+
+    public void incKnowledge(double time) {
+        this.knowledge = this.knowledge + this.timeConversion(time);
+    }
+
+    public void incCreativity(double time) {
+        this.creativity = this.creativity + this.timeConversion(time);
+    }
+
+    public void incComprehensibility(double time) {
+        this.comprehensibility = this.comprehensibility + this.timeConversion(time);
+    }
+
+    public void incMusicalAbility(double time) {
+        this.musicalAbility = this.musicalAbility + this.timeConversion(time);
 
     }
 
-    public void incKnowledge() {
-
+    public void incAppearance(double time) {
+        this.appearance = this.appearance + this.timeConversion(time);
     }
 
-    public void incCreativity() {
-
+    public void incPhysicality(double time) {
+        this.physicality = this.physicality + this.timeConversion(time);
+    }
+    public void incFitness(double time) {
+        this.fitness = this.fitness + this.timeConversion(time);
     }
 
-    public void incComprehensibility() {
-
+    public void incSociability(double time) {
+        this.sociability = this.sociability + this.timeConversion(time);
     }
 
-    public void incMusicalAbility() {
-
+    public void incFriendliness(double time) {
+        this.friendliness = this.friendliness + this.timeConversion(time);
     }
 
-    public void incAppearance() {
-
+    public void incExpressiveness(double time) {
+        this.expressiveness = this.expressiveness + this.timeConversion(time);
     }
 
-    public void incPhysicality() {
-
-    }
-    public void incFitness() {
-
+    public void incConfidence(double time) {
+        this.confidence = this.confidence + this.timeConversion(time);
     }
 
-    public void incSociability() {
-
+    public void incConcentration(double time) {
+        this.concentration = this.concentration + this.timeConversion(time);
     }
 
-    public void incFriendliness() {
-
+    public void incSentimentality(double time) {
+        this.sentimentality = this.sentimentality + this.timeConversion(time);
     }
 
-    public void incExpressiveness() {
-
+    public void incShoppingImpulsiveness(double time) {
+        this.shoppingImpulsiveness = this.shoppingImpulsiveness + this.timeConversion(time);
     }
 
-    public void incConfidence() {
-
+    public void incOtakuness(double time) {
+        this.otakuness = this.otakuness + this.timeConversion(time);
     }
-
-    public void incConcentration() {
-
-    }
-
-    public void incSentimentality() {
-
-
-    }
-
-    public void incShoppingImpulsiveness() {
-
-    }
-
-    public void incOtakuness() {
-
-    }
-
-
 }
