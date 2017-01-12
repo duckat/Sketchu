538264dc803ac3f9c49a101a3d11a0b3f10c7e31
klee166 <klee166@jhu.edu>
2017-01-12 08:20:15 -0500

    Temporary Backgroud

--- app/src/main/java/com/honeybin/sketchu/MainActivity.java
+++ app/src/main/java/com/honeybin/sketchu/MainActivity.java
@@ -30,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
           }
         });
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
