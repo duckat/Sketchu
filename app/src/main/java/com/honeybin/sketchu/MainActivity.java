64da0b67a3b0e736a770a0a7472fa962eb0f145f
klee166 <klee166@jhu.edu>
2017-01-18 08:17:02 -0500

    fixed text font, and made interface

--- app/src/main/java/com/honeybin/sketchu/MainActivity.java
+++ app/src/main/java/com/honeybin/sketchu/MainActivity.java
@@ -8,6 +8,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
