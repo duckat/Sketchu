f4a883dbafa0b660268400d66a88afa533c55cf8
klee166 <klee166@jhu.edu>
2017-01-23 06:17:57 -0500

    Finish button now works

--- app/src/main/java/com/honeybin/sketchu/EventCursorAdapter.java
+++ app/src/main/java/com/honeybin/sketchu/EventCursorAdapter.java
@@ -5,31 +5,55 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class EventCursorAdapter extends CursorAdapter {
+
+    private final Context ctx;
+    private Button tagButton;
+    private TextView name;
+    private EventsDBHelper events_db;
     public EventCursorAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
+        ctx = context;
     }
 
     // The newView method is used to inflate a new view and return it,
     // you don't bind any data to the view at this point.
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+    public View newView(Context context, final Cursor cursor, ViewGroup parent) {
         return LayoutInflater.from(context).inflate(R.layout.event_list_item, parent, false);
     }
+
+
 
     // The bindView method is used to bind all data to a given view
     // such as setting the text on a TextView.
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, final Cursor cursor) {
+        final int itemId = cursor.getInt(cursor.getColumnIndex("_id"));
         // Find fields to populate in inflated template
-        TextView name = (TextView) view.findViewById(R.id.nameCursor);
+        name = (TextView) view.findViewById(R.id.nameCursor);
         // Extract properties from cursor
         String nameString = cursor.getString(cursor.getColumnIndexOrThrow("name"));
         // Populate fields with extracted properties
         name.setText(nameString);
-        //tvPriority.setText(String.valueOf(priority));
+        events_db = new EventsDBHelper(ctx);
+
+        tagButton = (Button) view.findViewById(R.id.completeButton);
+        tagButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                events_db.deleteEvent(itemId);
+                cursor.requery();
+                notifyDataSetChanged();
+            }
+        });
+
     }
 }
